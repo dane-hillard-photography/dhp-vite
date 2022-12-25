fb18aa93c3c8e190df7dd85f6ae96f47047eefc3
Dane Hillard <github@danehillard.com>
2022-12-25 14:39:08 -0500

    Add contact form (#5)
    * Add contact form
    
    * Fix type for button type
    
    * Fix form styling and attributes
    
    * Connect form action route
    
    * Try lowercase post method
    
    * Add robots.txt file
    
    * Add headers

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {useHead} from "@vueuse/head"
 import {useRoute} from "vue-router"
+import Button from "../components/Button.vue";
 
 const route = useRoute()
 const pageTitle = 'Contact | Dane Hillard Photography'
@@ -25,7 +26,59 @@ useHead({
     If you are interested in setting up a portrait session or have any questions
     regarding me, my equipment, or what a session with me might be like, please contact me using the form below.
   </p>
+
+  <div class="form">
+    <div />
+    <form name="contact" netlify netlify-honeypot="pwned" action="/thanks/" method="post">
+      <label class="form__label" for="first_name">First name</label>
+      <input class="form__control" placeholder="First name *" name="first_name" id="first_name" type="text" required>
+  
+      <label class="form__label" for="last_name">Last name</label>
+      <input class="form__control" placeholder="Last name *" name="last_name" id="last_name" type="text" required>
+  
+      <label class="form__label" for="email">Email</label>
+      <input class="form__control" placeholder="Email *" name="email" id="email" type="email" required>
+  
+      <label class="form__label" for="phone">Phone number</label>
+      <input class="form__control" placeholder="Phone" name="phone" id="phone" type="tel" required>
+  
+      <label class="form__label" for="Subject">Subject</label>
+      <input class="form__control" placeholder="Subject *" name="subject" id="subject" type="text" required>
+  
+      <label class="form__label" for="Message">Message</label>
+      <textarea class="form__control" placeholder="Message *" name="message" id="message" required></textarea>
+  
+      <Button type="submit">Send</Button>
+    </form>
+    <div />
+  </div>
 </template>
 
 <style scoped>
+.form {
+  display: grid;
+  grid-template-columns: auto minmax(250px, 500px) auto;
+}
+
+.form__label {
+  position: absolute;
+  left: -10000px;
+  width: 1px;
+  height: 1px;
+  top: auto;
+  overflow: hidden;
+}
+
+.form__control {
+  display: block;
+  border-radius: 2px;
+  font-family: var(--font-family-body);
+  padding: var(--spacing-0-5-x);
+  width: 100%;
+}
+
+.form__control:focus-visible {
+  outline: 1px solid var(--color-accent);
+  box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6);
+}
 </style>
---
+++ src/views/ContactConfirmation.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {useHead} from "@vueuse/head"
+
+const pageTitle = 'Thanks | Dane Hillard Photography'
+useHead({
+  title: pageTitle,
+  meta: [
+    { name: "robots", content: 'noindex' },
+  ]
+})
+</script>
+
+<template>
+  <h1>Thank you!</h1>
+
+  <p>
+    Thanks for contacting me! If you requested further contact, I'll reach out shortly.
+  </p>
+</template>
+
+<style scoped>
+</style>
